1ea6427d9068c93aa390be2bdf73233549569660
Benoît JULLIOT <b.julliot@octo.com>
2023-11-09 10:50:18 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -9,22 +9,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TripServiceTest {
     @Test
-    public void shouldDoSomething() throws Exception {
+    public void when_user_without_trip_then_return_empty_trips_list() throws Exception {
         // Given
         User user = new User();
-        TripService tripService = new TripService(new UserServiceForTest());
+        User userConnected = new User();
+        TripService tripService = new TripService(new UserServiceForTest(userConnected));
 
         // When
         List<Trip> tripsOfGivenUser = tripService.getTripsByUser(user);
 
         // Then
-        assertThat(tripsOfGivenUser).isEqualTo("Not an exception");
+        assertThat(tripsOfGivenUser).isEmpty();
     }
+    @Test
+    public void when_user_without_trip_then_return_list() throws Exception {
+        // Given
+        User userParameter = new User();
+        User userConnected = new User();
+        userParameter.addFriend(userConnected);
+
+        TripService tripService = new TripService(new UserServiceForTest(userConnected));
+
+        // When
+        List<Trip> tripsOfGivenUser = tripService.getTripsByUser(userParameter);
+
+        // Then
+        assertThat(tripsOfGivenUser).isEmpty();
+    }
+
+
 }
 
 class UserServiceForTest implements UserServiceInterface {
+
+    private User user;
+
+    public UserServiceForTest(User user) {
+        this.user = user;
+    }
+
     @Override
     public User getLoggedUser() {
+        return user;
+    }
+
+    public User getFriends(){
         return new User();
     }
 }
